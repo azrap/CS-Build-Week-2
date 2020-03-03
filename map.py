234fc7eb7f955e1e73f1bf1eccc133f64a01fc95
Jeff <theta_158@yahoo.com>
2020-03-03 17:52:40 -0500

    added map traversal

--- map.py
+++ map.py
@@ -1,10 +1,12 @@
+import json
 import requests
 from time import sleep
 from decouple import config
 
 
-headers = config("auth")
-url = config("url")
+url = 'https://lambda-treasure-hunt.herokuapp.com/api/adv'
+TOKEN = config('token')
+headers = {'Authorization': 'Token ' + TOKEN}
 
 # Reverse directions
 reverse_dir = {'n': 's', 's': 'n', 'e': 'w', 'w': 'e'}
@@ -23,10 +25,10 @@ graph = {}
 
 # Movement payload for post request
 payload = {
-    'n': {'direction': 'n'},
-    's': {'direction': 's'},
-    'e': {'direction': 'e'},
-    'w': {'direction': 'w'}
+    'n': {"direction": "n"},
+    's': {"direction": "s"},
+    'e': {"direction": "e"},
+    'w': {"direction": "w"}
 }
 
 # While rooms visited is less than total rooms...
@@ -34,16 +36,12 @@ while len(rooms) < 500-1:
     # Keep track of previous room
     prev_room = None
     # Inspect current room
-    r = requests.get(f"{url}/init", headers=headers)
+    r = requests.get(url=url + '/init', headers=headers)
+    sleep(5)
+    print('new room:', r.json())
     current_room = r.json()['room_id']
-    cooldown = r.json()['cooldown']
-    # Wait for cooldown
-    if cooldown < 15:
-        sleep(cooldown + 15)
-        print('Waiting...')
-    else:
-        sleep(cooldown)
-        print('Waiting...')
+    print('current_room:', current_room)
+
     # If room hasn't been visited...
     if current_room not in rooms:
         rooms[current_room] = {
@@ -58,32 +56,60 @@ while len(rooms) < 500-1:
 
         # Add exits for current room to rooms
         exits[current_room] = r.json()['exits']
+        print('List of exits for the room', exits[current_room])
         # Grab last direction traveled
-        last_dir = reverse_path[-1]
-        # Remove last exit from exits
-        exits[current_room].remove(last_dir)
-        # Initialize graph room
-        graph[current_room] = {'n': '?', 's': '?', 'e': '?', 'w': '?'}
-        # Update graph for previous room
         if prev_room is not None:
+            print('prev_room:', prev_room)
+            last_dir = reverse_path[-1]
+            # Remove last exit from exits
+            exits[current_room].remove(last_dir)
+            print('Exits after removing last direction:', exits[current_room])
+            # Initialize graph room
+            graph[current_room] = {'n': '?', 's': '?', 'e': '?', 'w': '?'}
+            # Update graph for previous room
             if graph[prev_room][last_dir] == '?':
                 graph[prev_room][last_dir] = current_room
                 graph[current_room][reverse_dir[last_dir]] = prev_room
+                print('Graph:', graph)
 
-    # While there's no more rooms to explore...
-    while len(exits[current_room]) < 1:
-        # Pop last direction in reverse_path
-        reverse = reverse_path.pop()
-        # Move in reverse direction
-        requests.post(f"{url}/move", headers=headers, data=payload[reverse])
+        # While there's no more rooms to explore...
+        while len(exits[current_room]) < 1:
+            # Pop last direction in reverse_path
+            reverse = reverse_path.pop()
+            # Move in reverse direction
+            requests.post(url + "/move", headers=headers,
+                          data=payload[reverse])
+            print('backtracking...')
 
     # Travel in first available exit direction in room
     exit_dir = exits[current_room].pop(0)
+    print('exit:', exit_dir)
     # Add reverse direction to reverse path
     reverse_path.append(reverse_dir[exit_dir])
+    print('Reverse Path:', reverse_path)
+    # Update previous room
+    prev_room = current_room
     # Travel
-    requests.post(f"{url}/move", headers=headers, data=payload[exit_dir])
+    data = json.dumps(payload[exit_dir])
+    print('Print json.dumps:', json.dumps(payload[exit_dir]))
+    # p = requests.post(url + '/move/', headers=headers,
+    #                   json={"direction": "n"})
+    try:
+        p = requests.post(url + '/move', headers=headers,
+                          data=data)
+        print('Post request:', p.json())
+    except requests.exceptions.RequestException as e:
+        print(e)
     print('Traveling...')
+    cooldown = p.json()['cooldown']
+    sleep(cooldown + 8)
+    # Wait for cooldown
+    # if cooldown < 15:
+    #     sleep(cooldown + 15)
+    #     print('Waiting...')
+    # else:
+    #     sleep(cooldown)
+    #     print('Waiting...')
 
 # Save graph to text file
 with open('graph.txt', 'w') as f:
