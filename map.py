8a8a39dc3d75ad97b48b4c68cbbe51fca48c1126
azrap <azraap@gmail.com>
2020-03-02 17:28:39 -0800

    automating some walking around

---
+++ map.py
@@ -0,0 +1,49 @@
+{"room_id": 0,
+ "title": "A brightly lit room",
+ "description": "You are standing in the center of a brightly lit room. You notice a shop to the west and exits to the north, south and east.",
+ "coordinates": "(60,60)",
+ "elevation": 0, "terrain":
+ "NORMAL",
+ "players": ["User 20673", "User 20734", "User 20728", "User 20726", "User 20644", "User 20729", "User 20645", "User 20716", "User 20699", "User 20731", "User 20701", "User 20732", "User 20730", "User 20725", "User 20723", "User 20722", "User 20721", "User 20720", "User 20719", "User 20718", "User 20717", "User 20711", "User 20710", "User 20708", "User 20707",
+                                                                                                                                                                                                                                                           "User 20706", "User 20705", "User 20704", "User 20703", "User 20702", "User 20700", "User 20697", "User 20696", "User 20694", "User 20692", "User 20691", "User 20690", "User 20688", "User 20687", "User 20686", "User 20682", "User 20680", "User 20676", "User 20675", "User 20674", "User 20672", "User 20671", "User 20669", "User 20666", "User 20665", "User 20664", "User 20663", "User 20662", "User 20657", "User 20655", "User 20654", "User 20652", "User 20651", "User 20650", "User 20646", "User 20642", "User 20641"],
+ "items": [],
+ "exits": ["n", "s", "e", "w"],
+ "cooldown": 1.0,
+ "errors": [],
+ "messages": []}
+
+{"room_id": 8,
+ "title": "A misty room",
+ "description": "You are standing on grass and surrounded by a dense mist. You can barely make out the exits in any direction.",
+ "coordinates": "(59,59)",
+ "elevation": 0,
+ "terrain": "NORMAL",
+ "players": [],
+ "items": [],
+ "exits": ["s", "w"],
+ "cooldown": 15.0,
+ "errors": [],
+ "messages": ["You have walked north."]}
+
+{
+    "room_id": 16,
+    "title": "A misty room",
+    "description": "You are standing on grass and surrounded by a dense mist. You can barely make out the exits in any direction.",
+    "coordinates": "(58,59)",
+    "elevation": 0,
+    "terrain": "NORMAL",
+    "players": [],
+    "items": [],
+    "exits": [
+        "n",
+        "e",
+        "w"
+    ],
+    "cooldown": 15,
+    "errors": [],
+    "messages": [
+        "You have walked west."
+    ]
+}
+
+
