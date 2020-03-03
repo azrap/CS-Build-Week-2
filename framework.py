3864d5cff42b4e7d7ad70156bb03415bba1f656d
azrap <azraap@gmail.com>
2020-03-02 17:38:01 -0800

    def init() and def move() added. A while loop to automate walking in a single direction

---
+++ framework.py
@@ -0,0 +1,53 @@
+import time
+import requests
+import json
+
+
+Token = "<INPUT YOUR TOKEN HERE>"
+
+url = "https://lambda-treasure-hunt.herokuapp.com/api"
+
+
+headers = {'Authorization': f'Token {Token}'}
+
+
+def init():
+    r = requests.get(
+        f'{url}/adv/init/', headers=headers)
+
+    data = r.json()
+    print(data)
+    print(data['items'])
+    if len(data['errors']) > 0:
+        print(data)
+        return False
+    return data
+
+
+payload = {"direction": "e"}
+exits = ["e"]
+
+
+def move(payload):
+    r_move = requests.post(
+        f'{url}/adv/move/', data=json.dumps(payload), headers=headers)
+    data = r_move.json()
+    print('data inside the move', data)
+    if len(data['errors']) > 0:
+        print(data)
+        return False
+    return data
+
+
+while "e" in exits:
+    data = move(payload)
+    print('data inside the loop', data)
+    if not data:
+        break
+
+    # if len(data['errors']) > 0:
+    #     print('errors', data)
+    #     break
+    # print(data['items'])
+    # exits = data['exits']
+    time.sleep(20)
